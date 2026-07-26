9c05e6752a318c4e93569559d9d01d5ccd81fde5
AdolfoRM <adolform1984@gmail.com>
2026-07-26 17:59:38 -0600

    removido volumeicon y solo se usara pavucontrol

--- MySetup.sh
+++ MySetup.sh
@@ -2,14 +2,13 @@
 
 # 1. Define las parejas de rutas: "ORIGEN DESTINO"
 ENLACES=(
-    "$HOME/DebianConfiguration/.dosbox $HOME/"
-    "$HOME/DebianConfiguration/.vimrc $HOME/.vimrc"
-    "$HOME/DebianConfiguration/.Xresources $HOME/.Xresources"
-    "$HOME/DebianConfiguration/Wallpapers $HOME/"
-    "$HOME/DebianConfiguration/.icewm $HOME/"
-    "$HOME/DebianConfiguration/.newsboat $HOME/"
-    "$HOME/DebianConfiguration/gtk-3.0 $HOME/.config/"	
-    "$HOME/DebianConfiguration/picom $HOME/.config/"
+    "$HOME/DotFiles/.dosbox $HOME/"
+    "$HOME/DotFiles/.vimrc $HOME/.vimrc"
+    "$HOME/DotFiles/.Xresources $HOME/.Xresources"
+    "$HOME/DotFiles/Wallpapers $HOME/"
+    "$HOME/DotFiles/.icewm $HOME/"
+    "$HOME/DotFiles/gtk-3.0 $HOME/.config/"	
+    "$HOME/DotFiles/picom $HOME/.config/"
 )
 
 # 2. Crear los enlaces simbólicos
